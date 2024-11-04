566e592d1bfdd6be0f0e4a1fb4daabec792e30e8
Subhadeep Roy <subha9.5roy350@gmail.com>
2024-11-05 00:17:32 +0530

    Update getNewBento.ts

--- getNewBento.ts
+++ getNewBento.ts
@@ -1,4 +1,4 @@
-const apiUrl = "https://opbento.edgexhq.tech/api/bento?name=Subhadeep%20Roy&githubURL=subhadeeproy3902&twitterURL=mvp_Subha&linkedinURL=subhadeep3902&imageUrl=https%3A%2F%2Fi.postimg.cc%2FwBtF29yR%2Fsubha.jpg&portfolioUrl=mvp-subha.me&z=3389f";
+const apiUrl = "https://opbento.edgexhq.tech/api/bento?name=Subhadeep%20Roy&githubURL=subhadeeproy3902&twitterURL=mvp_Subha&linkedinURL=subhadeep3902&imageUrl=https%3A%2F%2Fi.postimg.cc%2FHsqvcD8Y%2Fsubha.jpg&portfolioUrl=mvp-subha.me&z=e37e3";
 interface BentoResponse {
   url: string;
 }
