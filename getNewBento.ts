ea0a475d3c4f5c89887e573ea54e2a45b1b5776e
Subhadeep Roy <subha9.5roy350@gmail.com>
2024-11-04 12:30:00 +0530

    Add files via upload

---
+++ getNewBento.ts
@@ -0,0 +1,22 @@
+const apiUrl = "https://opbento.vercel.app/api/bento?name=Subhadeep%20Roy&githubURL=subhadeeproy3902&twitterURL=mvp_Subha&linkedinURL=subhadeep3902&imageUrl=https%3A%2F%2Fi.postimg.cc%2F4Nc9BK6J%2Fsubha.jpg&portfolioUrl=mvp-subha.me";
+interface BentoResponse {
+  url: string;
+}
+
+const fetchBentoUrl = async (apiUrl: string): Promise<string> => {
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data: BentoResponse = await response.json();
+    return data.url;
+  } catch (error) {
+    console.error("Error fetching Bento URL:", error);
+    throw error;
+  }
+};
+
+
+// @ts-expect-error
+fetchBentoUrl(apiUrl);
